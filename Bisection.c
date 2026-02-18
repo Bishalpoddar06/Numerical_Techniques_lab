71f203b5800ed1fd06ecb9f7351aff103d034618
Bishalpoddar06 <poddarbishal1994@gmail.com>
2026-02-18 10:27:17 +0530

    hii

--- Bisection.c
+++ Bisection.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <time.h>
 
-/* Define the function */
+
 double f(double x)
 {
     return cos(x) - x * exp(x);
@@ -22,7 +22,6 @@ int main()
     printf("Enter the tolerance value: ");
     scanf("%lf", &tol);
 
-    /* Check for valid interval */
     if (f(a) * f(b) >= 0)
     {
         printf("Invalid interval! f(a) and f(b) must have opposite signs.\n");
@@ -48,7 +47,7 @@ int main()
 
     time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-    /* Output section */
+  
     printf("\nRoot of the equation = %.6f\n", c);
     printf("Number of iterations = %d\n", iterations);
     printf("Execution time = %lf seconds\n", time_taken);
